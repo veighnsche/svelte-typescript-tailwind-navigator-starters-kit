2ac3faae2957f873885574d5fb5fcfae3d7e4008
Vince Paul Liem <vincepaul.liem@gmail.com>
2022-03-26 10:01:13 +0100

    Add files via upload
    init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let name = 'world';
+</script>
+
+<h1>Hello {name}!</h1>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
